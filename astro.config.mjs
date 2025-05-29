7ea48d09a86a96874b4eddca692c75524bc620ce
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-05-29 15:01:49 +0200

    remove tailwind and convert to custom css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alpakasoelde.at',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: 'https://alpakasoelde.at'
 });
