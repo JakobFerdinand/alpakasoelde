d9cfdda4b8905cf0ec5b0c849c65d1b73228bc9b
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-05-29 13:56:05 +0200

    Update site for Alpakasölde (#102)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://diermair.at',
+  site: 'https://alpakasoelde.at',
   vite: {
     plugins: [tailwindcss()]
   }
