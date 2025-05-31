5fb992a30a50e9638fb95830dd671738db6a9637
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-05-31 10:53:19 +0200

    Store contact messages in table

--- Api/SendContactFunction.cs
+++ Api/SendContactFunction.cs
@@ -1,5 +1,10 @@
+using System;
 using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure;
+using Azure.Data.Tables;
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -15,12 +20,51 @@ namespace Api
             _logger = loggerFactory.CreateLogger<SendContactFunction>();
         }
 
+        private record ContactInput(string Name, string Email, string Message);
+
+        private class ContactEntity : ITableEntity
+        {
+            public string PartitionKey { get; set; } = "messages";
+            public string RowKey { get; set; } = Guid.NewGuid().ToString();
+            public string? Name { get; set; }
+            public string? Email { get; set; }
+            public string? Message { get; set; }
+            public DateTimeOffset? Timestamp { get; set; }
+            public ETag ETag { get; set; } = ETag.All;
+        }
+
         [Function("SendContact")]
         public async Task<HttpResponseData> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
         {
             var body = await new StreamReader(req.Body).ReadToEndAsync();
             _logger.LogInformation("Received data: {Body}", body);
+
+            ContactInput? input = JsonSerializer.Deserialize<ContactInput>(body);
+            if (input is not null)
+            {
+                var tableUrl = Environment.GetEnvironmentVariable("MESSAGES_TABLE");
+                if (!string.IsNullOrEmpty(tableUrl))
+                {
+                    var client = new TableClient(new Uri(tableUrl), new DefaultAzureCredential());
+                    await client.CreateIfNotExistsAsync();
+                    var entity = new ContactEntity
+                    {
+                        Name = input.Name,
+                        Email = input.Email,
+                        Message = input.Message
+                    };
+                    await client.AddEntityAsync(entity);
+                }
+                else
+                {
+                    _logger.LogError("MESSAGES_TABLE environment variable not set");
+                }
+            }
+            else
+            {
+                _logger.LogError("Invalid request body");
+            }
 
             var res = req.CreateResponse(System.Net.HttpStatusCode.OK);
             await res.WriteStringAsync("ok");
