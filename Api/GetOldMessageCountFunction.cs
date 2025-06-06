e93d52e9dbe8f7f7541712cd85d7e24d722127e3
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-06-06 15:31:35 +0200

    Add old message count function and display on admin dashboard (#192)

---
+++ Api/GetOldMessageCountFunction.cs
@@ -0,0 +1,27 @@
+using Azure.Data.Tables;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Api;
+
+public class GetOldMessageCountFunction(ILoggerFactory loggerFactory)
+{
+    private readonly ILogger _logger = loggerFactory.CreateLogger<GetOldMessageCountFunction>();
+
+    [Function("get-old-message-count")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    {
+        string? connectionString = Environment.GetEnvironmentVariable("MessageStorageConnection");
+        TableClient tableClient = new(connectionString, "messages");
+
+        DateTimeOffset threshold = DateTimeOffset.UtcNow.AddMonths(-6);
+        int count = tableClient.Query<MessageEntity>(m => m.Timestamp < threshold).Count();
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(new { Count = count }).ConfigureAwait(false);
+        return response;
+    }
+}
