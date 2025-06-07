b7adad35940d5f256fa698e5ae64765511724f31
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-06-07 13:26:45 +0200

    Add function to store alpacas (#213)

---
+++ Api/AlpakaEntity.cs
@@ -0,0 +1,15 @@
+namespace Api;
+
+using Azure;
+using Azure.Data.Tables;
+
+public sealed class AlpakaEntity : ITableEntity
+{
+    public required string Name { get; set; }
+    public required string Geburtsdatum { get; set; }
+
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string PartitionKey { get; set; } = "AlpakaPartition";
+    public string RowKey { get; set; } = Guid.NewGuid().ToString();
+}
