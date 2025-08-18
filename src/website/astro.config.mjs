ac66dbf95d4d774f1f0907a78ddc5162cb5489ea
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-08-18 05:58:12 +0000

    Fix Produkte images

--- src/website/astro.config.mjs
+++ src/website/astro.config.mjs
@@ -3,8 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alpakasoelde.at',
-  vite: {
-    assetsInclude: ['**/*.HEIC', '**/*.heic']
-  }
+  site: 'https://alpakasoelde.at'
 });
