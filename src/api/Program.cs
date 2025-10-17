63c1836e73ed9dd1faa9a6e5cde74cec282ef414
Jakob Ferdinand Wegenschimmel <jakob.wegenschimmel@rzl.at>
2025-10-17 10:41:58 +0200

    buildfix

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -7,7 +7,7 @@ var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
-        string? connectionString = Environment.GetEnvironmentVariable(WebsiteApi.EnvironmentVariables.StorageConnection);
+        string? connectionString = Environment.GetEnvironmentVariable(Api.EnvironmentVariables.StorageConnection);
         services.AddSingleton(_ => new TableServiceClient(connectionString));
         services.AddSingleton(_ => new BlobServiceClient(connectionString));
     })
